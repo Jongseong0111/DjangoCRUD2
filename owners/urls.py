71fed657c5ad06b3109a8b79c00cfdb4d3a4bcc8
임종성 <qkqndudnxld@gmail.com>
2021-07-20 14:00:38 +0900

    CRUD2 : GET Method

--- owners/urls.py
+++ owners/urls.py
@@ -1,8 +1,12 @@
+#from wecode23.crud2.crud2.owners.views import GetDogOwner
 from django.urls import path
 
-from owners.views import OwnerRegister, DogRegister
+from owners.views import *
 
 urlpatterns = [
-	path('', OwnerRegister.as_view()),
+	path('owners', OwnerRegister.as_view()),
     path('dogs', DogRegister.as_view()),
+    path('getdog', GetDogs.as_view()),
+    path('getowner', GetOwners.as_view()),
+    path('getdogowner', GetDogOwner.as_view()),
 ]
--- owners/views.py
+++ owners/views.py
@@ -32,3 +32,53 @@ class DogRegister(View):
         )
 
         return JsonResponse({'Message':'CREATED'}, status=201)
+
+class GetOwners(View):
+    def get(self, request):
+        owners = Owner.objects.all()
+        results=[]
+        for owner in owners:
+            results.append(
+                {
+                    "name" : owner.name,
+                    "email": owner.email,
+                    "age"  : owner.age
+                }
+            )
+        return JsonResponse({'resutls':results}, status=200)
+
+class GetDogs(View):
+    def get(self, request):
+        dogs = Dog.objects.all()
+        results=[]
+        for dog in dogs:
+            results.append(
+                {
+                    "name"   : dog.name,
+                    "age"    : dog.age,
+                    "owner"  : dog.owner.name
+                }
+            )
+        return JsonResponse({'resutls':results}, status=200)
+
+class GetDogOwner(View):
+    def get(self, request):
+        owners = Owner.objects.all()
+        results=[]
+
+        for owner in owners:
+            list= []
+            for dog in Dog.objects.filter(owner = owner).values('name', 'age'):
+                list.append(dog)
+            results.append(
+                {
+                    "name" : owner.name,
+                    "age"  : owner.age,
+                    "dogs" : list
+                }
+            )
+        
+        return JsonResponse({'resutls':results}, status=200)
+    
+
+
